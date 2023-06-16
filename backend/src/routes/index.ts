532c20d2fd2abbcfdc00c35ad32b53962a093905
UmarNazaket <umarnazaket97@gmail.com>
2023-06-17 02:13:04 +0500

    port added

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,6 @@
 import express,{Express} from 'express';
-import taskRoutes from './routes/taskRoutes';
-import userRoutes from './routes/userRoutes'
+import taskRoutes from '../routes/taskRoutes';
+import userRoutes from '../routes/userRoutes'
 
 const router: Express = express();
 

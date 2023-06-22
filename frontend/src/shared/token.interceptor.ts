52fa4c45c6325f488d2141dc5d55af73f0994437
UmarNazaket <umarnazaket97@gmail.com>
2023-06-22 19:16:49 +0500

    interceptor added for adding token

---
+++ frontend/src/shared/token.interceptor.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Get the token from wherever you store it (e.g., localStorage)
+    const token = localStorage.getItem('token');
+
+    // Add the token to the request headers
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    }
+
+    return next.handle(request);
+  }
+}
